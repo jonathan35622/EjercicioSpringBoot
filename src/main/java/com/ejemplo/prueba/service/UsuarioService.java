45bae3ebc2f2075d35745f792ae39dec3408be81
Jonathan <jony35622@gmail.com>
2021-01-28 17:30:21 -0300

    modifico repository. Agrego Optional, creo controlador y endpoint

--- src/main/java/com/ejemplo/prueba/service/UsuarioService.java
+++ src/main/java/com/ejemplo/prueba/service/UsuarioService.java
@@ -1,16 +1,20 @@
 package com.ejemplo.prueba.service;
 
 import com.ejemplo.prueba.entity.Usuario;
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface UsuarioService {
 
     Usuario save(Usuario usuario);
     List<Usuario> findAll();
-
-    Usuario userFindById(Long id);
+    Page<Usuario> findAll(Pageable pageable);
+    Optional<Usuario> userFindById(Long id);
 
 
 
